1f838135d1e399d3ea5ce2c0c430dfba8270f4e3
Noe LE CARDUNER <E146294Q@U-INF-J-C001-29.iut-nantes.univ-nantes.prive>
2015-10-12 10:09:22 +0200

    Noé

---
+++ class/pion.class.php
@@ -0,0 +1,28 @@
+<?php
+
+
+  class pion{
+
+    private $couleur;
+
+
+
+  function __construct($coul){
+    $this -> couleur = $coul;
+  }
+
+
+
+  function getCouleur(){
+    return $this -> $couleur;
+  }
+
+
+
+
+
+
+
+
+
+  }
