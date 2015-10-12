1d634341f701c83a7abc1b70620ecf171fa57e5d
supernoono <E146294Q@U-INF-J-C001-29.iut-nantes.univ-nantes.prive>
2015-10-12 10:45:25 +0200

    heheheheh

---
+++ entropie.php
@@ -0,0 +1,21 @@
+<?php
+
+
+for($i = 0; $i<=5; $i++){
+
+echo " <button type=". "radio" . ">" . "Couleur" . "</button>";
+echo " <button type=". "button" . ">" . "Couleur" . "</button>";
+echo " <button type=". "button" . ">" . "Couleur" . "</button>";
+echo " <button type=". "button" . ">" . "Couleur" . "</button>";
+echo " <button type=". "button" . ">" . "Couleur" . "</button>" . "<br>";
+
+}
+
+
+
+
+
+
+
+
+?>
