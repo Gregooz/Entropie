9bf64a47a9e80285b39761f96f595609de7a5318
Gregooz <gregoire.decamp@gmail.com>
2015-10-14 14:05:45 +0200

    Ajout de la classe case

---
+++ class/case.class.php
@@ -0,0 +1,18 @@
+<?php
+
+	class case{
+
+		private $positionX;
+		private $positionY;
+		private $pion;
+
+
+		function __construct($X, $Y, $pi){
+			$this -> positionX = $X;
+			$this -> positionY = $Y;
+			$this -> pion = $pi;
+		}
+	}
+
+
+?>
