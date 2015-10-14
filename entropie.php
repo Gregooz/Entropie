8f9b59255d17f315ff86e88383bde7fa0d7de2ce
supernoono <E146294Q@U-IUT-J-C004-03.iut-nantes.univ-nantes.prive>
2015-10-14 09:14:22 +0200

    fdef

--- entropie.php
+++ entropie.php
@@ -1,21 +1,45 @@
 <?php
 
+if(isset($_GET['var1']) == True){
 
-for($i = 0; $i<=5; $i++){
+echo "<table border="."1px". ">";
 
-echo " <button type=". "radio" . ">" . "Couleur" . "</button>";
-echo " <button type=". "button" . ">" . "Couleur" . "</button>";
-echo " <button type=". "button" . ">" . "Couleur" . "</button>";
-echo " <button type=". "button" . ">" . "Couleur" . "</button>";
-echo " <button type=". "button" . ">" . "Couleur" . "</button>" . "<br>";
+for($i=0;$i<5;$i++){
+	$y=0;
+	echo "<tr>";
+	echo "<td>". "<a href=?var1=". $_GET['var1'] . "&var2=". $i.$y .">". $i.$y++ . "</a></td>";
+	echo "<td>". "<a href=?var1=". $_GET['var1'] . "&var2=". $i.$y .">". $i.$y++ . "</a></td>";
+	echo "<td>". "<a href=?var1=". $_GET['var1'] . "&var2=". $i.$y .">". $i.$y++ . "</a></td>";
+	echo "<td>". "<a href=?var1=". $_GET['var1'] . "&var2=". $i.$y .">". $i.$y++ . "</a></td>";
+	echo "<td>". "<a href=?var1=". $_GET['var1'] . "&var2=". $i.$y .">". $i.$y++ . "</a></td>";
+	echo "</tr>";
+
+
+}
+echo "</table>";
+}
+
+else {
+echo "<table border="."1px". ">";
+
+
+for($i=0;$i<5;$i++){
+	$y=0;
+	echo "<tr>";
+	echo "<td>". "<a href=". "?var1=". $i.$y . ">". $i.$y++ . "</a></td>";
+	echo "<td>". "<a href=". "?var1=". $i.$y . ">". $i.$y++ . "</a></td>";
+	echo "<td>". "<a href=". "?var1=". $i.$y . ">". $i.$y++ . "</a></td>";
+	echo "<td>". "<a href=". "?var1=". $i.$y . ">". $i.$y++ . "</a></td>";
+	echo "<td>". "<a href=". "?var1=". $i.$y . ">". $i.$y++ . "</a></td>";
+	echo "</tr>";
+
+
+}
+echo "</table>";
 
 }
 
 
-
-
-
-
-
-
 ?>
+
+
